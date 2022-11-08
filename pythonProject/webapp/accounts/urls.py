ee2907593c5c00b48a086f44aeab0e691a585389
Suryajith R <suryajithr1998@gmail.com>
2022-11-08 22:32:22 +0530

    registration fixed

--- pythonProject/webapp/accounts/urls.py
+++ pythonProject/webapp/accounts/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-
+    path('',views.home,name='home'),
     path('reg/',views.register,name='register'),
 ]
--- pythonProject/webapp/accounts/views.py
+++ pythonProject/webapp/accounts/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User ,auth
-# Create your views here.
+from .models import Patient
 
-
+def home(request):
+    return render(request, 'home.html')
 
 def register(request):
     if request.method == 'POST':
-        full_name = request.POST['full_name']
-        user_name = request.POST['user_name']
-        email = request.POST['email']
-        phone_num = request.POST['phone_num']
-        dob = request.POST['dob']
-        house_name = request.POST['house_name']
-        street = request.POST['street']
-        city = request.POST['city']
-        pincode= request.POST['pincode']
-        password1= request.POST['password1']
-        password2 = request.POST['password2']
-        gender = request.POST['gender']
-        medicine=request.POST['medicine']
+        user=Patient()
+        user.full_name = request.POST['full_name']
+        user.user_name = request.POST['user_name']
+        user.email = request.POST['email']
+        user.phone_num = request.POST['phone_num']
+        user.dob = request.POST['dob']
+        user.house_name = request.POST['house_name']
+        user.street = request.POST['street']
+        user.city = request.POST['city']
+        user.pincode= request.POST['pincode']
+        user.password= request.POST['password1']
+        user.gender = request.POST['gender']
+        user.medicine=request.POST['med']
 
 
-        user=User.object.create_user(user_name=user_name,password=password1,email=email,full_name=full_name,phone_num=phone_num,dob=dob,house_name=house_name,street=street,city=city,pincode=pincode,gender=gender,medicine=medicine)
+        # user=Patient.object.create_user(user_name=user_name,password=password1,email=email,full_name=full_name,phone_num=phone_num,dob=dob,house_name=house_name,street=street,city=city,pincode=pincode,gender=gender,medicine=medicine)
         user.save()
         print("user_created")
         return render(request, 'home.html')
 
     else:
         return render(request,'register.html')
+
